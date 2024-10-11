6ff700a26142f02cee6ae700a160d5ee629d9214
kishikishore2863 <kishikishore.2863@gmail.com>
2024-10-11 23:50:04 +0530

    commit

---
+++ src/main/java/com/example/SpringApi/service/UserService.java
@@ -0,0 +1,34 @@
+package com.example.SpringApi.service;
+
+import com.example.SpringApi.api.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class UserService {
+    private List<User> userList;
+
+    public UserService(){
+        userList = new ArrayList<>();
+        User user1 = new User(1,"kishore",21,"kn331065@gmai.com");
+        User user2 = new User(1,"kishore",21,"kn331065@gmai.com");
+        User user3 = new User(1,"kishore",21,"kn331065@gmai.com");
+        userList.addAll(Arrays.asList(user1,user2,user3));
+    }
+
+    public Optional<User> getUser(Integer id) {
+        Optional<User> optional = Optional.empty();
+        for (User user: userList) {
+            if(id == user.getId()){
+                optional = Optional.of(user);
+                return optional;
+            }
+        }
+        return optional;
+    }
+}
